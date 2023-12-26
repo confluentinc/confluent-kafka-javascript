adbdc2022f03391577d4dc682e261772a39cceee
Milind L <miluthra@confluent.io>
2023-12-26 17:53:58 +0530

    Add alpine build: WIP

---
+++ .semaphore/build-docker.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+apk add -U ca-certificates openssl ncurses coreutils python3 make gcc g++ libgcc linux-headers grep util-linux binutils findutils perl patch musl-dev bash
+cd /v
+npm install
+make test
