5629aeaebdfe084c5e9ecc80b2175b0d38a15a8b
Robert Yokota <rayokota@gmail.com>
2024-08-22 21:05:28 -0700

    Fix test

--- test/schemaregistry/serde/avro.spec.ts
+++ test/schemaregistry/serde/avro.spec.ts
@@ -1,4 +1,4 @@
-import {describe, it} from '@jest/globals';
+import {describe, expect, it} from '@jest/globals';
 import {ClientConfig} from "../../../schemaregistry/rest-service";
 import {AvroDeserializer, AvroSerializer} from "../../../schemaregistry/serde/avro";
 import {newClient, SerdeType} from "../../../schemaregistry/serde/serde";
@@ -17,12 +17,16 @@ describe('AvroSerializer', () => {
       doubleField: 45.67,
       stringField: 'hi',
       boolField: true,
-      bytesField: new Uint8Array([1, 2]),
+      bytesField: new Buffer([1, 2]),
     }
     let bytes = await ser.serialize("topic1", obj)
 
     let deser = new AvroDeserializer(client, SerdeType.VALUE, {})
     let obj2 = await deser.deserialize("topic1", bytes)
-    console.log(obj2)
+    expect(obj2.intField).toEqual(obj.intField);
+    expect(obj2.doubleField).toBeCloseTo(obj.doubleField, 0.001);
+    expect(obj2.stringField).toEqual(obj.stringField);
+    expect(obj2.boolField).toEqual(obj.boolField);
+    expect(obj2.bytesField).toEqual(obj.bytesField);
   })
 })
