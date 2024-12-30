c902e11507c9926bec3caee9a6df47854f39c675
Milind L <miluthra@confluent.io>
2024-12-30 11:52:25 +0530

    Update alpine post-install

--- ci/post-install/install-alpine.sh
+++ ci/post-install/install-alpine.sh
@@ -1,17 +1,7 @@
 #!/bin/sh
 set -e
 
-apk add -U bash curl
-
-node_version="$1"
 library_version="$2"
-
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-
-nvm install $node_version
 
 # Only for logging purposes
 node -v
