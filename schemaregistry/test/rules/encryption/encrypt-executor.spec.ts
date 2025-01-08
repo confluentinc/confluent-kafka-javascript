0d6330ae71c8af16bca2c982c27f2f694b325769
Robert Yokota <rayokota@gmail.com>
2025-01-08 13:30:21 -0800

    Add test

--- schemaregistry/test/rules/encryption/encrypt-executor.spec.ts
+++ schemaregistry/test/rules/encryption/encrypt-executor.spec.ts
@@ -12,11 +12,11 @@ describe('FieldEncryptionExecutor', () => {
     const config = new Map<string, string>();
     config.set('key', 'value');
     executor.configure(clientConfig, config);
-    // configure with same args should be fine
+    // configure with same args is fine
     executor.configure(clientConfig, config);
     const config2 = new Map<string, string>();
     config2.set('key2', 'value2');
-    // configure with addition config keys should be fine
+    // configure with additional config keys is fine
     executor.configure(clientConfig, config);
 
     const clientConfig2: ClientConfig = {
@@ -26,7 +26,7 @@ describe('FieldEncryptionExecutor', () => {
     expect(() => executor.configure(clientConfig2, config)).toThrowError()
 
     const config3  = new Map<string, string>();
-    config3.set('key', 'value2');
+    config3.set('key', 'value3');
     expect(() => executor.configure(clientConfig, config3)).toThrowError()
   })
 })
