ecc9be52dc15ba8f8c10b5ae919bcc39f50c4035
Charles Lowell <cowboyd@frontside.com>
2025-03-26 15:56:46 -0500

    Topic NAN -> NAPI

--- src/topic.cc
+++ src/topic.cc
@@ -8,9 +8,7 @@
  */
 
 #include <string>
-#include <vector>
 
-#include "src/common.h"
 #include "src/connection.h"
 #include "src/topic.h"
 
@@ -29,10 +27,11 @@ namespace NodeKafka {
  * @sa NodeKafka::Connection
  */
 
-Topic::Topic(std::string topic_name, RdKafka::Conf* config):
-  m_topic_name(topic_name),
-  m_config(config) {
+void Topic::Setup(std::string topic_name, RdKafka::Conf *config) {
+  m_topic_name = topic_name;
+
   // We probably want to copy the config. May require refactoring if we do not
+  m_config = config;
 }
 
 Topic::~Topic() {
@@ -45,7 +44,7 @@ std::string Topic::name() {
   return m_topic_name;
 }
 
-Baton Topic::toRDKafkaTopic(Connection* handle) {
+template <class T> Baton Topic::toRDKafkaTopic(Connection<T>* handle) {
   if (m_config) {
     return handle->CreateTopic(m_topic_name, m_config);
   } else {
@@ -76,38 +75,34 @@ Baton offset_store (int32_t partition, int64_t offset) {
 
 Napi::FunctionReference Topic::constructor;
 
-void Topic::Init(Napi::Object exports) {
+void Topic::Init(const Napi::Env &env, Napi::Object exports) {
   Napi::HandleScope scope(env);
 
-  Napi::FunctionReference tpl = Napi::Function::New(env, New);
-  tpl->SetClassName(Napi::String::New(env, "Topic"));
-
-
-  InstanceMethod("name", &NodeGetName),
+  Napi::Function Topic = DefineClass(env, "Topic", {
+      InstanceMethod("name", &Topic::NodeGetName),
+    });
 
   // connect. disconnect. resume. pause. get meta data
-  constructor.Reset((tpl->GetFunction(Napi::GetCurrentContext()))
-    );
+  constructor.Reset(Topic);
 
-  (exports).Set(Napi::String::New(env, "Topic"),
-    tpl->GetFunction(Napi::GetCurrentContext()));
+  exports.Set(Napi::String::New(env, "Topic"), Topic);
 }
 
-void Topic::New(const Napi::CallbackInfo& info) {
+Topic::Topic(const Napi::CallbackInfo &info): ObjectWrap<Topic>(info) {
   Napi::Env env = info.Env();
   if (!info.IsConstructCall()) {
     Napi::Error::New(env, "non-constructor invocation not supported").ThrowAsJavaScriptException();
-    return env.Null();
+    return;
   }
 
   if (info.Length() < 1) {
     Napi::Error::New(env, "topic name is required").ThrowAsJavaScriptException();
-    return env.Null();
+    return;
   }
 
   if (!info[0].IsString()) {
     Napi::Error::New(env, "Topic name must be a string").ThrowAsJavaScriptException();
-    return env.Null();
+    return;
   }
 
   RdKafka::Conf* config = NULL;
@@ -119,61 +114,39 @@ void Topic::New(const Napi::CallbackInfo& info) {
     std::string errstr;
     if (!info[1].IsObject()) {
       Napi::Error::New(env, "Configuration data must be specified").ThrowAsJavaScriptException();
-      return env.Null();
+      return;
     }
 
-    config = Conf::create(RdKafka::Conf::CONF_TOPIC, (info[1].ToObject(Napi::GetCurrentContext())), errstr);  // NOLINT
+    config = Conf::create(RdKafka::Conf::CONF_TOPIC, info[1].ToObject(), errstr);  // NOLINT
 
     if (!config) {
       Napi::Error::New(env, errstr.c_str()).ThrowAsJavaScriptException();
-      return env.Null();
+      return;
     }
   }
 
-  std::string parameterValue = info[0].As<Napi::String>(.To<Napi::String>());
-  std::string topic_name(*parameterValue);
+  std::string parameterValue = info[0].ToString();
+  std::string topic_name(parameterValue);
 
-  Topic* topic = new Topic(topic_name, config);
-
-  // Wrap it
-  topic->Wrap(info.This());
-
-  // Then there is some weird initialization that happens
-  // basically it sets the configuration data
-  // we don't need to do that because we lazy load it
-
-  return info.This();
+  this->Setup(topic_name, config);
 }
 
-// handle
-
-Napi::Object Topic::NewInstance(Napi::Value arg) {
-  Napi::Env env = arg.Env();
-  Napi::EscapableHandleScope scope(env);
-
-  const unsigned argc = 1;
-
-  Napi::Value argv[argc] = { arg };
-  Napi::Function cons = Napi::Function::New(env, constructor);
-  Napi::Object instance =
-    Napi::NewInstance(cons, argc, argv);
-
-  return scope.Escape(instance);
-}
-
-Napi::Value Topic::NodeGetName(const Napi::CallbackInfo& info) {
+Napi::Value Topic::NodeGetName(const Napi::CallbackInfo &info) {
+  Napi::Env env = info.Env();
   Napi::HandleScope scope(env);
 
-  Topic* topic = ObjectWrap::Unwrap<Topic>(info.This());
+  Topic* topic = this;
 
-  return Napi::New(env, topic->name());
+  return Napi::String::From(env, this->name());
 }
 
-Napi::Value Topic::NodePartitionAvailable(const Napi::CallbackInfo& info) {
+Napi::Value Topic::NodePartitionAvailable(const Napi::CallbackInfo &info) {
+  return info.Env().Null();
   // @TODO(sparente)
 }
 
 Napi::Value Topic::NodeOffsetStore(const Napi::CallbackInfo& info) {
+  return info.Env().Null();
   // @TODO(sparente)
 }
 
--- src/topic.h
+++ src/topic.h
@@ -23,16 +23,15 @@ namespace NodeKafka {
 
 class Topic : public Napi::ObjectWrap<Topic> {
  public:
-  static void Init(const Napi::Env&, Napi::Object);
-  static Napi::Object NewInstance(Napi::Value arg);
+  static void Init(const Napi::Env &, Napi::Object);
+  Topic(const Napi::CallbackInfo& info);
 
   template <class T> Baton toRDKafkaTopic(Connection<T> *handle);
 
  protected:
-  static Napi::FunctionReference constructor;
-  static void New(const Napi::CallbackInfo& info);
+  static Napi::FunctionReference constructor;  
 
-  static Napi::Value NodeGetMetadata(const Napi::CallbackInfo& info);
+  Napi::Value NodeGetMetadata(const Napi::CallbackInfo& info);
 
   // TopicConfig * config_;
 
@@ -40,15 +39,15 @@ class Topic : public Napi::ObjectWrap<Topic> {
   std::string name();
 
  private:
-  Topic(std::string, RdKafka::Conf *);
+  void Setup(std::string, RdKafka::Conf *);
   ~Topic();
 
   std::string m_topic_name;
   RdKafka::Conf * m_config;
 
-  static Napi::Value NodeGetName(const Napi::CallbackInfo& info);
-  static Napi::Value NodePartitionAvailable(const Napi::CallbackInfo& info);
-  static Napi::Value NodeOffsetStore(const Napi::CallbackInfo& info);
+  Napi::Value NodeGetName(const Napi::CallbackInfo& info);
+  Napi::Value NodePartitionAvailable(const Napi::CallbackInfo& info);
+  Napi::Value NodeOffsetStore(const Napi::CallbackInfo& info);
 };
 
 }  // namespace NodeKafka
