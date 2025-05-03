7db3ea6441c4ca9d24b33ff5263e18ca4e6f9658
Robert Yokota <rayokota@gmail.com>
2025-05-03 16:17:30 -0700

    Add tests

--- schemaregistry/serde/serde.ts
+++ schemaregistry/serde/serde.ts
@@ -9,7 +9,6 @@ import {
 } from "../schemaregistry-client";
 import {RuleRegistry} from "./rule-registry";
 import {ClientConfig} from "../rest-service";
-import {parse, stringify} from "uuid"
 import {BufferWrapper, MAX_VARINT_LEN_64} from "./buffer-wrapper";
 import {IHeaders} from "../../types/kafkajs";
 
@@ -24,6 +23,11 @@ export const MAGIC_BYTE_V1 = Buffer.alloc(1, 1)
 
 export const KEY_SCHEMA_ID_HEADER = '__key_schema_id'
 export const VALUE_SCHEMA_ID_HEADER = '__value_schema_id'
+
+const byteToHex: string[] = [];
+for (let i = 0; i < 256; ++i) {
+  byteToHex.push((i + 0x100).toString(16).slice(1));
+}
 
 export class SchemaId {
   schemaType: string
@@ -45,7 +49,7 @@ export class SchemaId {
       this.id = payload.subarray(1, 5).readInt32BE(0)
       totalBytesRead = 5
     } else if (magicByte.equals(MAGIC_BYTE_V1)) {
-      this.guid = stringify(payload.subarray(1, 17))
+      this.guid = this.stringifyUuid(payload.subarray(1, 17))
       totalBytesRead = 17
     } else {
       throw new SerializationError(
@@ -58,6 +62,29 @@ export class SchemaId {
       totalBytesRead += bytesRead
     }
     return totalBytesRead
+  }
+
+  stringifyUuid(arr: Uint8Array, offset = 0) {
+    return (byteToHex[arr[offset + 0]] +
+      byteToHex[arr[offset + 1]] +
+      byteToHex[arr[offset + 2]] +
+      byteToHex[arr[offset + 3]] +
+      '-' +
+      byteToHex[arr[offset + 4]] +
+      byteToHex[arr[offset + 5]] +
+      '-' +
+      byteToHex[arr[offset + 6]] +
+      byteToHex[arr[offset + 7]] +
+      '-' +
+      byteToHex[arr[offset + 8]] +
+      byteToHex[arr[offset + 9]] +
+      '-' +
+      byteToHex[arr[offset + 10]] +
+      byteToHex[arr[offset + 11]] +
+      byteToHex[arr[offset + 12]] +
+      byteToHex[arr[offset + 13]] +
+      byteToHex[arr[offset + 14]] +
+      byteToHex[arr[offset + 15]]).toLowerCase();
   }
 
   idToBytes() : Buffer {
@@ -76,11 +103,24 @@ export class SchemaId {
     if (this.guid == null) {
       throw new SerializationError('Schema guid is not set')
     }
-    const guidBuffer = Buffer.from(parse(this.guid!))
+    const guidBuffer = Buffer.from(this.parseUuid(this.guid!))
     if (this.messageIndexes != null) {
       return Buffer.concat([MAGIC_BYTE_V1, guidBuffer, this.writeMessageIndexes(this.messageIndexes)])
     }
     return Buffer.concat([MAGIC_BYTE_V1, guidBuffer])
+  }
+
+  parseUuid(uuid: string): Uint8Array {
+    let v;
+    return Uint8Array.of(
+      (v = parseInt(uuid.slice(0, 8), 16)) >>> 24,
+      (v >>> 16) & 0xff,
+      (v >>> 8) & 0xff, v & 0xff,
+      (v = parseInt(uuid.slice(9, 13), 16)) >>> 8, v & 0xff,
+      (v = parseInt(uuid.slice(14, 18), 16)) >>> 8, v & 0xff,
+      (v = parseInt(uuid.slice(19, 23), 16)) >>> 8, v & 0xff,
+      ((v = parseInt(uuid.slice(24, 36), 16)) / 0x10000000000) & 0xff,
+      (v / 0x100000000) & 0xff, (v >>> 24) & 0xff, (v >>> 16) & 0xff, (v >>> 8) & 0xff, v & 0xff);
   }
 
   readMessageIndexes(payload: Buffer): [number, number[]] {
