7d20ea4e8a628ad23d760cbac29e6e84ddd86f14
Emanuele Sabellico <esabellico@confluent.io>
2025-10-16 14:06:19 +0200

    Configurable batch size

--- examples/performance/performance-primitives.js
+++ examples/performance/performance-primitives.js
@@ -19,6 +19,9 @@ module.exports = {
     newCompatibleProducer,
 };
 
+
+const MAX_BATCH_SIZE = process.env.MAX_BATCH_SIZE ? +process.env.MAX_BATCH_SIZE : null;
+
 function baseConfiguration(parameters) {
     let ret = {
         'client.id': 'kafka-test-performance',
@@ -142,6 +145,10 @@ function newCompatibleConsumer(parameters, eachBatch) {
     const autoCommitOpts = autoCommit > 0 ? 
         { 'enable.auto.commit': true, 'auto.commit.interval.ms': autoCommit } :
         { 'enable.auto.commit': false };
+    const jsOpts = {};
+    if (eachBatch && MAX_BATCH_SIZE !== null) {
+        jsOpts['js.max.batch.size'] = MAX_BATCH_SIZE;
+    }
 
     let groupId = eachBatch ? process.env.GROUPID_BATCH : process.env.GROUPID_MESSAGE;
     if (!groupId) {
@@ -153,6 +160,7 @@ function newCompatibleConsumer(parameters, eachBatch) {
         'auto.offset.reset': 'earliest',
         'fetch.queue.backoff.ms': '100',
         ...autoCommitOpts,
+        ...jsOpts,
     });
     return new CompatibleConsumer(consumer);
 }
