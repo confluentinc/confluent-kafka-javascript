e4a9ec9e2875cca86263ba9ad40c2e7d839f78ee
Constantin Dimitrov <constantind@users.noreply.github.com>
2026-09-23 12:41:04 +0200

    Fix: segfault in consumer loop due to use after free in workers thread- #34
    When NodeDisconnect calls worker.WorkComplete() and Destroy() at src/kafka-consumer.cc#L1396 sets the callback to null, while the thread is inside the loop stopping or starting in a race, creating "use after free" segmentation fault in the message callback handler on Linux.
    
    Closes: #34

--- src/workers.h
+++ src/workers.h
@@ -107,7 +107,14 @@ class MessageWorker : public ErrorAwareWorker {
     }
 
     for (unsigned int i = 0; i < message_queue.size(); i++) {
-      HandleMessageCallback(message_queue[i], RdKafka::ERR_NO_ERROR);
+      if (callback && !callback->IsEmpty()) {
+        HandleMessageCallback(message_queue[i], RdKafka::ERR_NO_ERROR);
+      } else {
+        // cleaning up heap allocations
+        if (message_queue[i] != NULL) {
+          delete message_queue[i];
+        }
+      }
 
       // we are done with it. it is about to go out of scope
       // for the last time so let's just free it up here. can't rely
@@ -115,7 +122,9 @@ class MessageWorker : public ErrorAwareWorker {
     }
 
     for (unsigned int i = 0; i < warning_queue.size(); i++) {
-      HandleMessageCallback(NULL, warning_queue[i]);
+      if (callback && !callback->IsEmpty()) {
+        HandleMessageCallback(NULL, warning_queue[i]);
+      }
     }
   }
 
